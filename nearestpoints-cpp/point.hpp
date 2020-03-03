5222f8c9094367a51fe5e4913b5faa717ce8478e
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2020-03-03 15:52:33 +0000

    some doc

--- nearestpoints-cpp/point.hpp
+++ nearestpoints-cpp/point.hpp
@@ -1,13 +1,16 @@
-#pragma once
-
 #include <vector>
+
+/** Given the Point struct and the function nearestPoints please fill in the
+ details. The function nearestPoints should return a new vector ( list ) with 
+ the `n` points out of the parameter `points` which are closed 
+ ( euclidean distance ) to `center`.
+
+ Please include tests for your implementation.
+*/
 
 struct Point {
 	double x;
 	double y;
-
-	Point(double xi, double yi) : x(xi), y(yi) {
-	}
 };
 
 std::vector<Point> nearestPoints(const Point& center, const std::vector<Point>& points, const size_t n);
