72652e249cbc56f6297008996f202e7e020e09ab
Atila Neves <atila.neves@gmail.com>
2020-05-28 16:30:36 +0200

    Refactored unit tests

--- nearestpoints-d/tests/tests.d
+++ nearestpoints-d/tests/tests.d
@@ -1,4 +1,5 @@
 import impl;
+import unit_threaded;
 import std.math : approxEqual;
 import std.format: format;
 import std.algorithm.iteration: map;
@@ -6,54 +7,51 @@ import std.algorithm.sorting: isSorted;
 
 
 unittest {
-	Point a = Point(1.0, 2.0);
-	Point b = Point(2.0, 1.0);
-	Point c = Point(-1.0, 2.0);
-	Point d = Point(-1.0, -2.0);
-	Point e = Point(0.0, 0.0);
+	const a = Point(1.0, 2.0);
+	const b = Point(2.0, 1.0);
+	const c = Point(-1.0, 2.0);
+	const d = Point(-1.0, -2.0);
+	const e = Point(0.0, 0.0);
 
 	{
-		double r0 = distance(a, b);
-		double r1 = distance(b, a);
-
-		assert(approxEqual(r0, r1), format("%s %s", r0, r1));
+		const r0 = distance(a, b);
+		const r1 = distance(b, a);
+        r0.should ~ r1;
 	}
 
 	{
-		double r0 = distance(a, c);
-		double r1 = distance(b, a);
-
-		assert(!approxEqual(r0, r1), format("%s %s", r0, r1));
+		const r0 = distance(a, c);
+		const r1 = distance(b, a);
+		r0.should.not ~ r1;
 	}
 
 	{
-		double r0 = distance(e, d);
-		double r1 = distance(e, a);
-
-		assert(approxEqual(r0, r1), format("%s %s", r0, r1));
+		const r0 = distance(e, d);
+		const r1 = distance(e, a);
+		r0.should ~ r1;
 	}
 }
 
 
 unittest {
 	import std.algorithm.comparison : equal;
-	auto p1 = pointRng(10, 1337);
-	auto p2 = pointRng(10, 1337);
-	assert(equal(p1, p2));
+	const p1 = pointRng(10, 1337);
+	const p2 = pointRng(10, 1337);
+	p1.should == p2;
 }
 
 
 unittest {
-	auto center = Point(0.0, 0.0);
-	auto rslt = nearestPoint(pointRng(2000, 1338), center, 10);
-	assert(rslt.length == 10);
+	const center = Point(0.0, 0.0);
+	const rslt = nearestPoint(pointRng(2000, 1338), center, 10);
+	rslt.length.should == 10;
 	assert(rslt.map!(it => distance(center, it)).isSorted);
 }
 
 
 unittest {
-	auto center = Point(0.0, 0.0);
-	auto rslt = nearestPoint(pointRng(7, 1338), center, 10);
-	assert(rslt.length == 7);
+	const center = Point(0.0, 0.0);
+	const rslt = nearestPoint(pointRng(7, 1338), center, 10);
+	rslt.length.should == 7;
 	assert(rslt.map!(it => distance(center, it)).isSorted);
 }
