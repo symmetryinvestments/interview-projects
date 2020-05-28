c6660e23e650de07c3e6459ed52c28770a423e2d
Atila Neves <atila.neves@gmail.com>
2020-05-28 16:37:03 +0200

    Delete implementation

--- nearestpoints-d/source/impl.d
+++ nearestpoints-d/source/impl.d
@@ -10,33 +10,7 @@ struct Point {
 
 
 Point[] nearestPoint(Input)(Input input, const(Point) center, size_t n) {
-	import std.algorithm.sorting : sort;
-	import std.array : array;
-	import std.algorithm.iteration : map;
-
-	static struct PointDis {
-		Point p;
-		double d;
-	}
-
-	PointDis[] tmp;
-
-	foreach(Point it; input) {
-		PointDis pd = PointDis(it, distance(center, it));
-		if(tmp.length < n) {
-			tmp ~= pd;
-			sort!((a, b) => a.d < b.d)(tmp);
-		} else {
-			foreach(ref p; tmp) {
-				if(pd.d < p.d) {
-					p = pd;
-					break;
-				}
-			}
-		}
-	}
-
-	return tmp.map!(it => it.p).array;
+	return [];
 }
 
 
